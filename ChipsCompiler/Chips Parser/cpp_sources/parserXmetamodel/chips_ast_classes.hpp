b72919773d11ffe1dc1d3911c0acc09ca289c4b4
NwaitDev <anna.galdel@protonmail.com>
2026-03-03 16:20:57 +0100

    partial update again

--- ChipsCompiler/Chips Parser/cpp_sources/parserXmetamodel/chips_ast_classes.hpp
+++ ChipsCompiler/Chips Parser/cpp_sources/parserXmetamodel/chips_ast_classes.hpp
@@ -1,7 +1,8 @@
 #ifndef ASTMETAMODEL_H
 #define ASTMETAMODEL_H
 #include <cstddef>
-#include <concepts>
+#include <variant>
+#include <vector>
 #include <functional>
 #include <string>
 #include <iostream>
@@ -11,7 +12,7 @@ namespace chips
     /**
      * Components that can be plugged in system section
      */ 
-    enum block_type {
+    enum class block_type {
         LOGICAL, // only using regular in/outputs
         PHYSICAL, // can use regular in/outputs, sensor/actuators and channel in/outputs
         OBJECT // can only be plugged to other objects with channel in/outputs
@@ -20,7 +21,7 @@ namespace chips
     /** 
      * Types of the elements that can appear in the preamble section
      */
-    enum definition_type {
+    enum class definition_type {
         NODE, // to define objects and physicals
         FUNCTION, // to define logical and physicals
         IMPLEMENTATION // to define node inheritance (work in progress, do not use)
@@ -30,7 +31,7 @@ namespace chips
      * Environments for the interpretation 
      * of direct values and variables
      */
-    enum rvalue_env {
+    enum class rvalue_env {
         PRIMITIVE, // each value is interpreted as a regular variable
         COLLECTIVE, // each value can be interpreted as either a value of its type or a "NULL" (stop value)
         SYSTEM // each value is interpreted as a regular variable until it is plugged. Then, it represents a constant dataflow
@@ -39,7 +40,7 @@ namespace chips
     /**
      * Primitive types of data that Chips handles
      */
-    enum dataflow_type {
+    enum class dataflow_type {
         INT, // regular integers
         FLOAT, // floating point values
         BOOL // booleans
@@ -48,7 +49,7 @@ namespace chips
     /**
      * Kind of signals to be distinguished when working with Cyber Physical Systems
      */
-    enum dataflow_kind {
+    enum class dataflow_kind {
         LOGICAL, // produced by any functional block or system variable
         PHYSICAL // specific to physical functional blocks
     };
@@ -56,7 +57,7 @@ namespace chips
     /**
      * Node specific variable kinds
      */
-    enum node_element {
+    enum class node_element {
         CHANNEL, // communication port to refer to when transmitting a signal between nodes
         CONTEXTUAL // variable shared by all the functional blocks supported by a node
     };
@@ -66,11 +67,11 @@ namespace chips
     class ast_node{
     public:
         void hello();
-        virtual void accept(visitor& v) = 0;
+        void accept(visitor& v);
     };
     class visitor{
     public:
-        virtual void visit(ast_node &node) = 0;
+        void visit(ast_node &node);
     };
 
 
@@ -86,61 +87,58 @@ namespace chips
     // When declared with a [integer expression]+ suffix,
     // it is of the given dimension(s).
 
-    template<enum rvalue_env> 
-    class array : public ast_node; // concrete
-    template<enum rvalue_env V>
-    class variable : public array<V>; // concrete
+    template<rvalue_env rve> 
+    class array : public ast_node{}; // concrete
+    template<rvalue_env rve>
+    class variable : public array<rve>{}; // concrete
 
 
     //////////////////// VARIABLE AST NODES ////////////////////////
 
-    class primitive_variable : public variable<chips::rvalue_env::PRIMITIVE>; // abstract
+    class primitive_variable : public variable<chips::rvalue_env::PRIMITIVE>{}; // abstract
     template<dataflow_type dft> 
-    class dataflow_primitive_variable : public primitive_variable; // concrete
+    class dataflow_primitive_variable : public primitive_variable{}; // concrete
 
-    class node_variable : public variable<chips::rvalue_env::PRIMITIVE>; // abstract
+    class node_variable : public variable<chips::rvalue_env::PRIMITIVE>{}; // abstract
     template<dataflow_type dft> 
-    class contextual_variable : public node_variable; // concrete
+    class contextual_variable : public node_variable{}; // concrete
 
-    class collective_variable : public variable<chips::rvalue_env::COLLECTIVE>; // abstract
+    class collective_variable : public variable<chips::rvalue_env::COLLECTIVE>{}; // abstract
     template<dataflow_type dft> 
-    class dataflow_collective_variable; // concrete
+    class dataflow_collective_variable{}; // concrete
 
 
-    class system_variable : public variable<chips::rvalue_env::SYSTEM>; // abstract
+    class system_variable : public variable<chips::rvalue_env::SYSTEM>{}; // abstract
     template<block_type bt> 
-    class block_variable : public system_variable; // concrete
+    class block_variable : public system_variable{}; // concrete
     template<dataflow_type dft> 
-    class dataflow_system_variable : public system_variable; // concrete
+    class dataflow_system_variable : public system_variable{}; // concrete
 
     ////////////////// STATEMENTS MANAGEMENT ////////////////////////
 
     /**
      * Statements that are encountered in different environments
      */
-    enum recuring_statements {
-        // generic
+    enum class recurring_statement {
+        // generic statements
         IF, // if ( bool expr ){ statements } [ else { statements } ]
         FOREACH, // for variable in iterable { statements }
         DECLARATION, // type identifier
         ASSIGNMENT, // identifier = expression
-        
-        // specific to system section
+        // system specific statements
         IMPLEMENTS, // identifier implements identifier using identifier
         PLUGGING, // identifier.identifier(identifier.identifier)
         FEEDING, // identifier.identifier(expression | identifier.identifier)
-        LINKING // link identifier to identifier
-
-        // specific to implementation section (work in progress, do not use)
-        ALIASING
-
+        LINKING, // link identifier to identifier
+        // implementation specific statements
+        ALIASING // specific to implementation section (work in progress, do not use)
     };
 
     /**
      * Environments that should allow to distinguish
      * the kind of statements that can be used in the code
      */
-    enum statement_env {
+    enum class statement_env {
         PRIMITIVE, // in the body of function definitions (init/then sections)
         COLLECTIVE, // in the body of collective primitive definitions
         SYSTEM, // in the system description
@@ -149,67 +147,67 @@ namespace chips
     };
 
 
-    template<enum statement_env, enum recuring_statements>
-    class statement : public ast_node; // abstract class
+    template<statement_env sttenv, recurring_statement recstt>
+    class statement : public ast_node{}; // abstract class
 
-    template<recuring_statements>
-    using system_statement = statement<statement_env::SYSTEM>; // abstract (by definition)
-    template<recuring_statements>
-    using node_statement = statement<statement_env::NODE>; // abstract (by definition)
-    template<recuring_statements>
-    using implementation_statement = statement<statement_env::IMPLEMENTATION>; // abstract (by definition)
-    template<recuring_statements>
-    using primitive_statement = statement<statement_env::PRIMITIVE>; // abstract (by definition)
-    template<recuring_statements>
-    using collective_statement = statement<statement_env::COLLECTIVE>; // abstract (by definition)
+    template<recurring_statement recstt>
+    using system_statement = statement<statement_env::SYSTEM, recstt>{}; // abstract (by definition)
+    template<recurring_statement recstt>
+    using node_statement = statement<statement_env::NODE, recstt>{}; // abstract (by definition)
+    template<recurring_statement recstt>
+    using implementation_statement = statement<statement_env::IMPLEMENTATION, recstt>{}; // abstract (by definition)
+    template<recurring_statement recstt>
+    using primitive_statement = statement<statement_env::PRIMITIVE, recstt>{}; // abstract (by definition)
+    template<recurring_statement recstt>
+    using collective_statement = statement<statement_env::COLLECTIVE, recstt>{}; // abstract (by definition)
 
 
     ////// Generic statements
     template<dataflow_type dft, statement_env stenv>
-    class dataflow_declaration : public statement<stenv, recuring_statements::DECLARATION>; // concrete
+    class dataflow_declaration : public statement<stenv, recurring_statement::DECLARATION>{}; // concrete
     template<dataflow_type dft, statement_env stenv>
-    class dataflow_assignment : public statement<stenv, recuring_statements::ASSIGNMENT>; // concrete
+    class dataflow_assignment : public statement<stenv, recurring_statement::ASSIGNMENT>{}; // concrete
 
     template<statement_env stenv>
-    class if_statement : public statement<stenv,recuring_statements::IF>; // concrete
+    class if_statement : public statement<stenv,recurring_statement::IF>{}; // concrete
     template<statement_env stenv>
-    class if_else_statement : public if_statement<stenv>; // concrete
+    class if_else_statement : public if_statement<stenv>{}; // concrete
     template<statement_env stenv>
-    class foreach_statement : public statement<stenv, recuring_statements::FOREACH>; // concrete
+    class foreach_statement : public statement<stenv, recurring_statement::FOREACH>{}; // concrete
     template<statement_env stenv>
-    class if_section; // concrete
+    class if_section{}; // concrete
     template<statement_env stenv>
-    class else_section; // concrete
+    class else_section{}; // concrete
 
     ////// System specific statements
     template<block_type bt>
-    class block_declaration : public statement<statement_env::SYSTEM , recuring_statements::DECLARATION>; // concrete
+    class block_declaration : public statement<statement_env::SYSTEM , recurring_statement::DECLARATION>{}; // concrete
 
-    class implements_statement : public system_statement<recuring_statements::IMPLEMENTS>; // concrete (work in progress, do not use)
+    class implements_statement : public system_statement<recurring_statement::IMPLEMENTS>{}; // concrete (work in progress, do not use)
 
-    class channel_plugging : public system_statement<recuring_statements::PLUGGING>; // concrete
+    class channel_plugging : public system_statement<recurring_statement::PLUGGING>{}; // concrete
 
     template<dataflow_kind dfk, dataflow_type dft>
-    class feeding_statement : public system_statement<recuring_statements::FEEDING>; // concrete
+    class feeding_statement : public system_statement<recurring_statement::FEEDING>{}; // concrete
 
-    class linking_statement : public system_statement<recuring_statements::LINKING>; // concrete
+    class linking_statement : public system_statement<recurring_statement::LINKING>{}; // concrete
 
     ////// Implementation specific statements (work in progress, do not use)
 
     template <enum node_element>
-    class aliasing_statement : public implementation_statement<recuring_statements::ALIASING>; // concrete
+    class aliasing_statement : public implementation_statement<recurring_statement::ALIASING>{}; // concrete
 
     ////// Node specific statements
     template<enum node_element, enum dataflow_type>
-    class node_element_declaration : public node_statement<recuring_statements::DECLARATION>; // concrete
+    class node_element_declaration : public node_statement<recurring_statement::DECLARATION>{}; // concrete
 
     ////////////////////////// DEFINITION PARAMETERS MANAGEMENT ///////////////////////////////
 
-    template<dataflow_kind, dataflow_type>
-    class function_parameter : public ast_node;
+    template<dataflow_kind dfk, dataflow_type dft>
+    class function_parameter : public ast_node{}; // concrete
     
     template<dataflow_type>
-    class collective_parameter : public ast_node;
+    class collective_parameter : public ast_node{}; // concrete
 
     ////////////////////////// DEFINITION OUTPUTS MANAGEMENT //////////////////////////////////
 
@@ -223,35 +221,115 @@ namespace chips
     };
 
     template<dataflow_kind, dataflow_type>
-    class function_output : public ast_node;
+    class function_output : public ast_node{}; // concrete
     
-    template<collective_output_kind>
-    class collective_output : public ast_node;
+    template<enum collective_output_kind>
+    class collective_output : public ast_node{}; // concrete
 
 
     ////////////////////////// DEFINITIONS MANAGEMENT ///////////////////////////////////////
 
-    class definition : public ast_node;
-    class with_section : public ast_node;
-    class init_section : public ast_node;
-    class then_section : public ast_node;
-    class collectiveops_section : public ast_node;
-    class accumulator_definition : public ast_node;
-    class node_definition : public definition;
-    class object_definition : public node_definition;
-    class function_definition : public defintion;
-    class accumulator_definition : public ast_node;
-    class accumulator_definition : public ast_node;
+    /**
+     * Different types of collective primitives
+     */
+    enum collective_function_type {
+        SPREAD, // generalization of the broadcast operation
+        COLLECT // generalization of the merge operation
+    };
+
+    class definition : public ast_node // abstract
+    {
+    private:
+        std::string m_name;
+    };
+
+    using node_statement_variant = std::variant<
+        node_statement<recurring_statement::IF>*,
+        node_statement<recurring_statement::FOREACH>*,
+        node_statement<recurring_statement::DECLARATION>*,
+        node_statement<recurring_statement::ASSIGNMENT>*>;
+
+    class with_section : public ast_node // concrete
+    {
+    private:
+        std::vector<node_statement_variant> m_statements;
+    };
+
+    using primitive_statement_variant = std::variant<
+        primitive_statement<recurring_statement::IF>*,
+        primitive_statement<recurring_statement::FOREACH>*,
+        primitive_statement<recurring_statement::DECLARATION>*,
+        primitive_statement<recurring_statement::ASSIGNMENT>*>;
+    
+    class init_section : public ast_node // concrete
+    {
+    private:
+        std::vector<primitive_statement_variant> m_statements;
+    };
+
+    class then_section : public ast_node // concrete
+    {
+    private:
+        std::vector<primitive_statement_variant> m_statements;
+    };
+
+    using collective_statement_variant = std::variant<
+        collective_statement<recurring_statement::IF>*,
+        collective_statement<recurring_statement::FOREACH>*,
+        collective_statement<recurring_statement::DECLARATION>*,
+        collective_statement<recurring_statement::ASSIGNMENT>*>;
+
+    class collectiveops_section : public ast_node //concrete
+    {
+    private:
+        std::vector<collective_statement_variant> m_statements;
+    };
+
+    using collective_parameter_variant = std::variant<
+        collective_parameter<dataflow_type::INT>*,
+        collective_parameter<dataflow_type::FLOAT>*,
+        collective_parameter<dataflow_type::BOOL>*>;
+        
+    class accumulator_definition : public ast_node
+    {
+    private:
+        
+    };
+    class node_definition : public definition{};
+    class object_definition : public node_definition{};
+
+
+    using function_parameter_variant = std::variant<
+        function_parameter<dataflow_kind::LOGICAL, dataflow_type::INT>*,
+        function_parameter<dataflow_kind::LOGICAL, dataflow_type::FLOAT>*,
+        function_parameter<dataflow_kind::LOGICAL, dataflow_type::BOOL>*>;
+
+    using function_output_variant = std::variant<
+        function_output<dataflow_kind::LOGICAL, dataflow_type::INT>*,
+        function_output<dataflow_kind::LOGICAL, dataflow_type::FLOAT>*,
+        function_output<dataflow_kind::LOGICAL, dataflow_type::BOOL>*>;
+        
+    class function_definition : public definition // abstract
+    {
+    private:
+        init_section init;
+        then_section then;
+        std::vector<function_parameter_variant> m_parameters;
+        std::vector<function_output_variant> m_parameters;
+    };
+    class logical_definition : public function_definition{}; // concrete
+    class physical_definition : public function_definition, public node_definition{};
+    class implementation_defintion : public definition{};
+    class collective_function_definition : public definition{};
 
 
 
 
     class program_node : public ast_node
     {
-    private:
         std::string m_filename;
-        preamble_section_node m_preamble;
-        system_section_node m_system;
+        preamble_section_node& m_preamble;
+        system_section_node& m_system;
 
     public:
         program_node(std::string filename, preamble_section_node &preamble, system_section_node &system);
