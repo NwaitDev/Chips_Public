063755f83c9d675ce20255a5c0dc1c2249cd1099
vitalfocheux <vitalfocheux@gmail.com>
2026-07-02 12:52:10 +0200

    sdghfbuisdbvqsojnbcnksnck,dspc,lqs,kdclqsnksk,lcnklsd,vckqdls,kfl,qksdlc,lmskocl

--- ChipsCompiler/FullCompileChain/setenv.sh
+++ ChipsCompiler/FullCompileChain/setenv.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
 export CHIPS_PATH=$(realpath ../..)
-<<<<<<< HEAD
 export CHIPSC_PATH="$CHIPS_PATH"/ChipsCompiler/FullCompileChain/Chips2BipTransformer
 export BIPC_PATH="$CHIPS_PATH"/ChipsCompiler/FullCompileChain/BipCompiler/distribution/build/bipc-latest/bin
-=======
-export CHIPSC_PATH="$CHIPS_PATH"/ChipsCompiler/FullCompileChain
->>>>>>> main
 cd BipCompiler/distribution ; source setenv.sh; cd -
 
 export BIP_PATH="$CHIPSC_PATH"/ChipsCompiler/FullCompileChain/BipCompiler/
